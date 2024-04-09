5d88682de7a7a11f48376b31432b2f049aaac9bc
Kacper <kacpeeer.mikolajczyk@gmail.com>
2024-04-09 09:13:26 +0200

    connect datebase

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const { port } = require('./config')
+const apiRouter = require('./routes/api')
 
-const apiRouter = require('./router/api')
+require('./db/mongoose')
+
 app.use('/', apiRouter)
 
 app.listen(port, function (req, res) {
